6c66cbae16846bae5ede1ed013908c60e30335b2
blurvblur-hash <blurvblur@gmail.com>
2026-02-01 17:47:28 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-sky-50 flex items-center justify-center text-xl text-sky-800 font-bold">
+      🎉 Salah 的可爱日历上线啦！正在加载完整版中…
+    </div>
+  );
+}
